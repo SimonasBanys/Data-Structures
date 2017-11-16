c096c403d3d4135386d2540434861d2714816dc4
Perkuns <31985858+Perkuns@users.noreply.github.com>
2017-11-16 12:44:50 +0000

    Add files via upload

---
+++ FlightItinerary.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+
+
+public class FlightItinerary
+{
+
+    public static void partA()
+    {
+        SimpleDirectedWeightedGraph<>;
+    }
+
+    public static void main(String[] args)  throws FileNotFoundException, FlightItineraryException
+    {
+        final String[] airlines = {"BA", "EK"};
+        FlightsReader reader = new FlightsReader(airlines);
+
+    }
+
+}
